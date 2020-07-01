1d44cfda1d923ef386f6811f7dcece0046c5395b
abioduncomms <amichael@commsworth.com>
2020-07-01 16:31:08 +0100

    changed port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const next = require('next')
-    
+
+const PORT = process.env.PORT
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
@@ -15,9 +16,9 @@ app.prepare()
   })
 
  
-  server.listen(3000, (err) => {
+  server.listen(PORT, (err) => {
     if (err) throw err
-    console.log('> Ready on http://localhost:3000')
+    console.log(`> Ready on http://localhost:${PORT}`)
   })
 })
 .catch((ex) => {
